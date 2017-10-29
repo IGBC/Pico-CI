0922390d82901fe086e6858237bcb834775b9fb8
SEGFAULT <fakespamtrap@gmail.com>
2017-10-30 00:02:00 +0100

    Added most of the process code, untested for now.

--- picoci.py
+++ picoci.py
@@ -1,34 +1,114 @@
 import logging
 import argparse
 import shutil
+from os import chdir, getcwd
+from shlex import split
 from flask import Flask, request
 from tempfile import makedtemp
-
+from subprocess import run, PIPE
 
 __version__ = '0.0.1-git'
 __description__ = 'Simple script to build and deploy git repos'
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST'])
+@app.route('/', methods=['POST'])
 def remote_update():
-    logger.getLogger('picoci.update')
-    logger.info('Recieved Request with data: %s' % request.form)
+    ''' Triggers updater from webhook '''
+    logger = logging.getLogger('picoci.update')
+    logger.debug('Recieved Request with data: %s' % request.form)
     return 'OK'
 
 def local_update():
+    ''' Triggers updater from local machine '''
+    logger = logging.getLogger('picoci.update')
+    logger.info('Update requested from local machine')
     build_and_deploy()
 
 def build_and_deploy():
-    #create a tempory dir to use as scratch disk
+    ''' clones source repo and runs build and deploy scripts '''
+    logger = logging.getLogger('picoci.bad')
+    
+    # Create a tempory dir to use as scratch disk
+    logging.debug('making temp dir')
     path = makedtemp()
-    if app.config['REPO_URL']:
-        
+    
+    # Switch into tempdir
+    logger.debug('cd_ing into %s' % path)
+    chdir(path)
+    
+    # If the repo is blank log an error and exit
+    if not app.config['REPO_URL']:
+        logger.fatal('No repository defined in config file. Cannot update.')
+        return 
+    
+    # Clone Repo
+    clone(path)
+    # Build App
+    build(path)
+    # deploy App
+    deploy(path)
+    
+    #Remove the temp dir again.
+    logger.debug('Removing temp dir' % path)
     shutil.rmtree(path)
+  
+def clone(path):
+    ''' Clones the repo
+        Returns success '''
+    logger = logging.getLogger('picoci.clone')
+    logger.info('Cloning %s' % app.config['REPO_URL'])
+    command = 'git clone %s %s' % (app.config['REPO_URL'], path)
+    logger.debug('clone command: %s' % command)        
+    return run_cmd(command, logger)
+        
+    
+def build(path):
+    ''' Runs the build step
+        Returns success '''
+    logger = logging.getLogger('picoci.build')
+    # Skip if no script defined
+    if not app.config['BUILD_SCRIPT']:
+        logger.debug('No build script listed, skipping.')
+        return True
+    
+    logger.info('Running build script')
+    command = './' + app.config['BUILD_SCRIPT']
+    logger.debug('build command: %s' % command)
+    return run_cmd(command, logger)
 
+def deploy(path):
+    ''' Runs the deploy step
+        Returns success '''
+    logger = logging.getLogger('picoci.deploy')
+    # Skip if no script defined
+    if not app.config['DEPLOY_SCRIPT']:
+        logger.debug('No deploy script listed, skipping.')
+        return True
+    
+    logger.info('Running deploy script')
+    command = './' + app.config['DEPLOY_SCRIPT']
+    logger.debug('deploy command: %s' % command)
+    return run_cmd(command, logger)
 
+def run_cmd(command, logger)
+    ''' Run shell command,
+    log the shit out of errors.
+        Returns success'''
+    # Run clone command and dump stdout into output
+    output = run(split(command), stdout=PIPE)
+    # If return code is not 0 (SUCCESS) log the shit out of what happened
+    if output.returncode:
+        logger.error('Command returned non 0 code %s. Stopping' % output.returncode)
+        logger.error('%s ARGS: "%s"' % (output.returncode, output.args))
+        logger.error('WK_DIR: "%s"' % getcwd())
+        logger.error('STDOUT: "%s"' % output.stdout)
+        return False
+    else:
+        return True
 
 def main():
+    ''' It's the entry point '''
     # Configure Argparse
     parser = argparse.ArgumentParser(description=__description__)
     parser.add_argument('config file', type=str)
